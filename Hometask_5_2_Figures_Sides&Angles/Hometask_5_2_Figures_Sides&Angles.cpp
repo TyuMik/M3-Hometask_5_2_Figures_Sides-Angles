14f343a90aa04ad36bff776e70c58618890ee5b3
Mikhail <miktyu88@gmail.com>
2024-02-10 22:40:54 +0300

    Correction 1

--- Hometask_5_2_Figures_Sides&Angles/Hometask_5_2_Figures_Sides&Angles.cpp
+++ Hometask_5_2_Figures_Sides&Angles/Hometask_5_2_Figures_Sides&Angles.cpp
@@ -9,12 +9,14 @@ public:
     std::string get_name() {
         return name;
     }
+    virtual void print_info() {
+        std::cout << get_name() << ": " << std::endl;
+    }
 };
 
 class Triangle : public Figure {
 protected:
-    int a, b, c;
-    int A, B, C;
+    int a, b, c, A, B, C;
 public:
     Triangle(int a, int b, int c, int A, int B, int C) {
         name = "Треугольник";
@@ -32,41 +34,40 @@ public:
     int get_B() { return B; }
     int get_C() { return C; }
 
-    void print_info(Triangle* tgl) {
-        std::cout << tgl->get_name() << ": " << std::endl;
+    void print_info() {
+        Figure::print_info();
         std::cout << "Стороны: " << std::endl;
-        std::cout << "a=" << tgl->get_a() << "; " << "b=" << tgl->get_b() << "; " << "c=" << tgl->get_c() << std::endl;
+        std::cout << "a=" << get_a() << "; " << "b=" << get_b() << "; " << "c=" << get_c() << std::endl;
         std::cout << "Углы: " << std::endl;
-        std::cout << "A=" << tgl->get_A() << "; " << "B=" << tgl->get_B() << "; " << "C=" << tgl->get_C() << std::endl;
+        std::cout << "A=" << get_A() << "; " << "B=" << get_B() << "; " << "C=" << get_C() << std::endl;
         std::cout << std::endl;
     }
 };
 
 class Triangle_p : public Triangle {
 public:
-    Triangle_p(int a, int b, int c, int A, int B) : Triangle(a, b, c, A, B, C = 90) {
+    Triangle_p(int a, int b, int c, int A, int B) : Triangle(a, b, c, A, B, 90) {
         name = "Прямоугольный треугольник";
     }
 };
 
 class Triangle_rb : public Triangle {
 public:
-    Triangle_rb(int a, int b, int A, int B) : Triangle(a, b, c = a, A, B, C = A) {
+    Triangle_rb(int a, int b, int A, int B) : Triangle(a, b, a, A, B, A) {
         name = "Равнобедренный треугольник";
     }
 };
 
 class Triangle_rs : public Triangle {
 public:
-    Triangle_rs(int a) : Triangle(a, b = a, c = a, A = 60, B = 60, C = 60) {
+    Triangle_rs(int a) : Triangle(a, a, a, 60, 60, 60) {
         name = "Равносторонний треугольник";
     }
 };
 
 class Quadrangle : public Figure {
 protected:
-    int a, b, c, d;
-    int A, B, C, D;
+    int a, b, c, d, A, B, C, D;
 public:
     Quadrangle(int a, int b, int c, int d, int A, int B, int C, int D) {
         name = "Четырёхугольник";
@@ -88,44 +89,47 @@ public:
     int get_C() { return C; }
     int get_D() { return D; }
 
-    void print_info(Quadrangle* qgl) {
-        std::cout << qgl->get_name() << ": " << std::endl;
+    void print_info() {
+        Figure::print_info(); 
         std::cout << "Стороны: " << std::endl;
-        std::cout << "a=" << qgl->get_a() << "; " << "b=" << qgl->get_b() << "; " << "c=" << qgl->get_c() << "; " << "d=" << qgl->get_d() << std::endl;
+        std::cout << "a=" << get_a() << "; " << "b=" << get_b() << "; " << "c=" << get_c() << "; " << "d=" << get_d() << std::endl;
         std::cout << "Углы: " << std::endl;
-        std::cout << "A=" << qgl->get_A() << "; " << "B=" << qgl->get_B() << "; " << "C=" << qgl->get_C() << "; " << "D=" << qgl->get_D() << std::endl;
+        std::cout << "A=" << get_A() << "; " << "B=" << get_B() << "; " << "C=" << get_C() << "; " << "D=" << get_D() << std::endl;
         std::cout << std::endl;
     }
 };
 
 class Parallelogram : public Quadrangle {
 public:
-    Parallelogram(int a, int b, int A, int B) : Quadrangle(a, b, c = a, d = b, A, B, C = A, D = B) {
+    Parallelogram(int a, int b, int A, int B) : Quadrangle(a, b, a, b, A, B, A, B) {
         name = "Параллелограмм";
     }
 };
 
 class Romb : public Parallelogram {
 public:
-    Romb(int a, int A, int B) : Parallelogram(a, b = a, A, B) {
+    Romb(int a, int A, int B) : Parallelogram(a, a, A, B) {
         name = "Ромб";
     }
 };
 
 class Parallel_p : public Parallelogram {
 public:
-    Parallel_p(int a, int b) : Parallelogram(a, b, A = 90, B = 90) {
+    Parallel_p(int a, int b) : Parallelogram(a, b, 90, 90) {
         name = "Прямоугольник";
     }
 };
 
 class Square : public Parallel_p {
 public:
-   Square(int a) : Parallel_p(a, b = a) {
+   Square(int a) : Parallel_p(a, a) {
         name = "Квадрат";
     }
 };
 
+void printInfo(Figure* f) {
+    f->print_info();
+};
 
 int main()
 {
@@ -135,11 +139,11 @@ int main()
     Triangle_p tp(10, 20, 30, 50, 60);
     Triangle_rb trb(10, 20, 50, 60);
     Triangle_rs trs(30);
-
-    t.print_info(&t);
-    tp.print_info(&tp);
-    trb.print_info(&trb);
-    trs.print_info(&trs);
+    
+    printInfo(&t);
+    printInfo(&tp);
+    printInfo(&trb);
+    printInfo(&trs);
 
     Quadrangle q(10, 20, 30, 40, 50, 60, 70, 80);
     Parallelogram p(20, 30, 80, 100);
@@ -147,11 +151,11 @@ int main()
     Romb r(30, 30, 40);
     Square s(20);
 
-    q.print_info(&q);
-    pp.print_info(&pp);
-    s.print_info(&s);
-    p.print_info(&p);
-    r.print_info(&r);
+    printInfo(&q);
+    printInfo(&pp);
+    printInfo(&s);
+    printInfo(&p);
+    printInfo(&r);
 
     return 0;
 }
